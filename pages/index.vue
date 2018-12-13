a6bbab302fdb6b241a208b6852959ac0e6002262
Katsuma Narisawa <face.the.music721@gmail.com>
2018-12-14 00:06:13 +0900

    Implement bases

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,30 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        smashbros
-      </h1>
-      <h2 class="subtitle">
-        A Generator for logo images of smashbros sp
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div id="root">
+    <Canvas
+      :width="canvasWidth"
+      :aspect-ratio="canvasAspectRatio"
+      :background-image="backgroundImage"
+    />
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Canvas from '~/components/Canvas'
+import backgroundImage from '~/assets/images/sansen2.jpeg'
 
 export default {
   components: {
-    Logo
+    Canvas
+  },
+  data() {
+    return {
+      backgroundImage: backgroundImage,
+      canvasWidth: 600,
+      canvasAspectRatio: 0.5661
+    }
   }
 }
 </script>
 
 <style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
