fbe9ab8ec7fe0af2da0acf40fb4eef52dd2d9cf2
Katsuma Narisawa <face.the.music721@gmail.com>
2018-12-14 02:46:59 +0900

    Implement UI (#3)

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,14 +13,6 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    script: [
-      {
-        innerHTML:
-          '(function(d) {var config = {kitId: "lfl6jjo",scriptTimeout: 3000,async: true},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src="https://use.typekit.net/"+config.kitId+".js";tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);',
-        type: 'text/javascript',
-        charset: 'utf-8'
-      }
-    ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,131 @@
-<template>
-  <div id="root">
-    <Canvas
+<template lang="pug">
+  #root
+    h1
+      | スマブラ参戦風
+      br
+      | 画像ジェネレーター
+
+    .uk-margin
+      .uk-form-custom
+          input(type="file" @change="onFileChanged" accept="image/*" multiple)
+          button.uk-button.uk-button-default(type=button)
+            span(uk-icon="icon: cloud-upload")
+            | 画像を選択
+
+    .uk-margin
+      label.uk-form-label(for="settings-name")
+        | 参戦者名
+      input#settings-name.uk-input(type="text" placeholder="(例) キングクルール" v-model="name")
+    .uk-margin
+      label.uk-form-label(for="settings-image-size")
+        | 画像サイズ
+      input#settings-image-size.uk-range(type="range" min="0" max="2" step="0.001" v-model="size")
+    .uk-margin
+      label.uk-form-label(for="settings-font-size")
+        | 文字サイズ
+      input#settings-font-size.uk-range(type="range" min="40" max="120" step="5" v-model="fontSize")
+
+    Canvas(
+      v-if="isCanvasVisible"
       :width="canvasWidth"
-      :aspect-ratio="canvasAspectRatio"
+      :height="canvasHeight"
       :background-image="backgroundImage"
-    />
-  </div>
+      :chara-image="image"
+      :name="name"
+      :size="parseFloat(size)"
+      :font-size="parseInt(fontSize)"
+    )
 </template>
 
 <script>
 import Canvas from '~/components/Canvas'
 import backgroundImage from '~/assets/images/sansen2.jpeg'
+import UIkit from 'uikit'
 
 export default {
   components: {
     Canvas
   },
   data() {
+    const canvasWidth = Math.min(600, window.innerWidth)
     return {
       backgroundImage: backgroundImage,
-      canvasWidth: 600,
-      canvasAspectRatio: 0.5661
+      canvasWidth: canvasWidth,
+      canvasAspectRatio: 0.5661,
+      name: '',
+      size: 1,
+      fontSize: 60 + canvasWidth / 300,
+      image: null,
+      isCanvasVisible: false
+    }
+  },
+  computed: {
+    canvasHeight() {
+      return this.canvasWidth * this.canvasAspectRatio
+    }
+  },
+  created() {
+    this.fetchFont(document)
+    setTimeout(() => {
+      this.isCanvasVisible = true
+    }, 500)
+  },
+  methods: {
+    fetchFont(d) {
+      var config = {
+          kitId: 'lfl6jjo',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h = d.documentElement,
+        t = setTimeout(function() {
+          h.className =
+            h.className.replace(/\bwf-loading\b/g, '') + ' wf-inactive'
+        }, config.scriptTimeout),
+        tk = d.createElement('script'),
+        f = false,
+        s = d.getElementsByTagName('script')[0],
+        a
+      h.className += ' wf-loading'
+      tk.src = 'https://use.typekit.net/' + config.kitId + '.js'
+      tk.async = true
+      tk.onload = tk.onreadystatechange = function() {
+        a = this.readyState
+        if (f || (a && a != 'complete' && a != 'loaded')) return
+        f = true
+        clearTimeout(t)
+        try {
+          Typekit.load(config)
+        } catch (e) {}
+      }
+      s.parentNode.insertBefore(tk, s)
+    },
+    onFileChanged(e) {
+      const file = e.target.files[0] || e.dataTransfer.files[0]
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const img = new Image()
+        img.src = window.URL.createObjectURL(file)
+
+        img.onload = () => {
+          this.size = Math.min(
+            (this.canvasWidth * 0.7) / img.width,
+            this.canvasHeight / img.height
+          )
+          this.image = img
+          return
+        }
+      }
+      reader.readAsDataURL(file)
     }
   }
 }
 </script>
 
 <style>
+h1 {
+  color: #040404;
+  font-family: 'source-han-sans-japanese' !important;
+}
 </style>
