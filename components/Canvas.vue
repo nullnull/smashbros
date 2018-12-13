1b881e3182c65aef371135a7456200207f92ed8c
Katsuma Narisawa <face.the.music721@gmail.com>
2018-12-14 00:40:29 +0900

    [WIP] Generate Logos (#2)
    Implement base to generate image

---
+++ components/Canvas.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="root">
+    <v-stage
+      ref="stage"
+      :config="{
+        width: width,
+        height: height,
+    }">
+      <v-layer ref="layer">
+        <v-image
+          :config="{
+            image: backgroundImageUrl,
+            width: width,
+            height: height,
+          }"
+        />
+      </v-layer>
+      <v-layer ref="layer">
+        <v-image
+          :config="{
+            draggable: true,
+            image: charaImageUrl
+          }"
+        />
+      </v-layer>
+      <v-layer
+        ref="layer">
+        <v-text
+          :config="{
+            text: 'ヒメ',
+            fontSize: 100,
+            fontFamily: 'source-han-sans-japanese',
+            fontWeight: 900,
+            x: 300,
+            y: 77,
+            draggable: true,
+            stroke: '#050505',
+            strokeWidth: 4,
+            rotation: 11,
+            fill: '#E2E2E2'
+          }"
+        />
+      </v-layer>
+    </v-stage>
+    <div>
+      ヒメ
+    </div>
+    <img id="image_png">
+  </div>
+</template>
+
+<script>
+import CharaImage from '~/assets/images/hime.png'
+
+export default {
+  props: {
+    width: {
+      type: Number,
+      required: true
+    },
+    aspectRatio: {
+      type: Number,
+      required: true
+    },
+    backgroundImage: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      charaImageUrl: null,
+      backgroundImageUrl: null
+    }
+  },
+  computed: {
+    height() {
+      return this.width * this.aspectRatio
+    }
+  },
+  created() {
+    if (process.browser) {
+      const backgroundImage = new Image()
+      backgroundImage.src = this.backgroundImage
+      backgroundImage.onload = () => {
+        // set image only when it is loaded
+        this.backgroundImageUrl = backgroundImage
+      }
+
+      const charaImage = new Image()
+      charaImage.src = CharaImage
+      charaImage.onload = () => {
+        this.charaImageUrl = charaImage
+      }
+    }
+  },
+  methods: {
+    saveAsImage(e) {
+      const stage = e.target.getStage()
+      document.getElementById('image_png').src = stage.toDataURL()
+    }
+  }
+}
+</script>
+
+<style>
+#root {
+  color: red;
+  font-family: 'source-han-sans-japanese';
+  font-size: 90px;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+  mode: 'spa',
 
   /*
   ** Headers of the page
@@ -12,6 +12,14 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    script: [
+      {
+        innerHTML:
+          '(function(d) {var config = {kitId: "lfl6jjo",scriptTimeout: 3000,async: true},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src="https://use.typekit.net/"+config.kitId+".js";tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);',
+        type: 'text/javascript',
+        charset: 'utf-8'
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
@@ -29,7 +37,10 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [{ src: '@/plugins/uikit', ssr: false }],
+  plugins: [
+    { src: '@/plugins/uikit', ssr: false },
+    { src: '@/plugins/konva', ssr: false }
+  ],
 
   /*
   ** Nuxt.js modules
